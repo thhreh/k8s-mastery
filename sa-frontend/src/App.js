a7eb679ddd480664e7d2fe35cc0634f8a1a101e2
Wei Yan <weiyan@Weis-Mac-Studio.local>
2024-04-02 13:13:42 -0700

    update hw5

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
+        fetch(process.env.REACT_APP_WEBAPP_URL+'/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
